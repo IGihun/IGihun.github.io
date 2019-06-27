5b46d62dde320c741be18b2fdc4f01a916a000ea
IGihun <gh07152@naver.com>
2019-06-27 17:57:23 +0900

    benz

---
+++ benz/js/ui.js
@@ -0,0 +1,18 @@
+
+
+window.onload = function(){
+
+	var navMenu = document.querySelector(".nav_menucover");
+	var modelbtn = document.querySelector(".model_btn");
+		modelbtn.addEventListener("click", function(e){
+		e.preventDefault();
+		if (navMenu.className === "nav_menucover") {
+			navMenu.classList.add("on");
+			
+		} else {
+			navMenu.classList.remove("on");
+		}
+	})
+	// var flowWord = document.querySelector(".flow");
+	// var strong = flowWord.querySelector
+}
