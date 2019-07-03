1f246fffea593e6d5af7c5f4fb8ebbade18ca1b3
gh07152@naver.com <g800326s>
2019-07-03 18:08:47 +0900

    7/3

--- benz/js/ui.js
+++ benz/js/ui.js
@@ -1,15 +1,47 @@
 
 
 window.onload = function(){
-	var navMenu = document.querySelector(".nav_menucover");
-	var modelbtn = document.querySelector(".model_btn");
-		modelbtn.addEventListener("click", function(e){
-		e.preventDefault();
-		if (navMenu.className === "nav_menucover") {
-			navMenu.classList.add("on");
-			
-		} else {
-			navMenu.classList.remove("on");
-		}
-	})
+	var navCover = document.querySelectorAll(".nav_menucover");
+	var navMenus = document.querySelectorAll(".nav_menucover > div");
+	var navMenuLis = document.querySelectorAll("nav > ul > li");
+	var navMenuBtn = document.querySelectorAll("nav > ul > li > a");
+
+	for(let i = 0; i < navMenuBtn.length; i++) {
+		navMenuBtn[i].onclick = (function(j){
+			return function(e){
+				e.preventDefault();
+				for(var k = 0; k < navMenuBtn.length; k++ ){
+					if (navMenus[k].classList.contains("on")) {
+						navMenus[k].classList.remove("on");
+						navMenuLis[k].classList.remove("on")
+						navCover.style.display = "none"
+					} 	
+				}
+				navMenus[j].classList.add("on");
+				navMenuLis[j].classList.add("on");
+				navCover.style.display = "block"
+			}
+		})(i);
+	}
+
+	var moedelMenuLis = document.querySelectorAll(".model_menu > li");
+	var moedelMenuBtn = document.querySelectorAll(".model_menu > li > a");
+	var modelPhotos = document.querySelectorAll(".model_photo > ul");
+
+	for(var i = 0; i < moedelMenuBtn.length ;i++) {
+		
+		moedelMenuBtn[i].onclick = (function(j){
+			return function(e){
+				e.preventDefault();
+				for(var k = 0; k < moedelMenuBtn.length; k++){
+					if(modelPhotos[k].classList.contains("on")){
+						modelPhotos[k].classList.remove("on");
+						moedelMenuLis[k].classList.remove("on")
+					}
+				}
+				moedelMenuLis[j].classList.add("on")
+				modelPhotos[j].classList.add("on")
+			}
+		})(i)
+	}
 }
